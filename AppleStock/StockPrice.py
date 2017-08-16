a97a3c06f4ee7f0742ffadf2404d59f86ed319b5
Anumeet Nepaul <anepaul@fc-md.umd.edu>
2017-08-16 01:37:53 -0400

    Optimized StockPrice python code

--- AppleStock/StockPrice.py
+++ AppleStock/StockPrice.py
@@ -1,27 +1,26 @@
 # source https://www.interviewcake.com/question/python/stock-price
 
 def get_max_profit(stock_prices):
-    if len(stock_prices) <= 1:
-        return 0
+    # Parameter validation
+    if len(stock_prices) < 2:
+        raise IndexError("Input paramter list does not contain atleast 2 values")
 
+    # Buy values constraint
     buy_val = stock_prices[0]
-    sell_val = stock_prices[1]
-    prof_val = sell_val - buy_val
+    # Maximizing Profit
+    max_prof = stock_prices[1] - buy_val
 
     for i in range(1, len(stock_prices)):
         stock_price = stock_prices[i]
-        # stop updating buy price before the last index
-        if stock_price < buy_val and i < len(stock_prices)-1:
-            buy_val = stock_price
-            sell_val = stock_prices[i+1]
 
-        elif stock_price > sell_val:
-            sell_val = stock_price
+        # Check current profit
+        profit = stock_price - buy_val
+        # Maximize profit
+        max_prof = max(profit, max_prof)
+        # Remember the largest previous profit but update to the lowest value seen
+        buy_val = min(stock_price, buy_val)
 
-        if (sell_val - buy_val) > prof_val:
-            prof_val = (sell_val - buy_val)
-
-    return prof_val
+    return max_prof
 
 stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
 print(get_max_profit(stock_prices_yesterday)) # return 6
