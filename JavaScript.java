9802de07893436a3b132933658c12472fbcfe615
Anumeet Nepaul <anepaul@fc-md.umd.edu>
2017-08-16 01:58:58 -0400

    Added generic script files for java, python, and cpp

---
+++ JavaScript.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static String myFunction(String arg) {
+        // write the body of your function here
+        return "running with " + arg;
+    }
+    public static void main(String[] args) {
+        // run your function through some test cases here
+        // remember: debugging is half the battle!
+        String testInput = "test input";
+        System.out.println(myFunction(testInput));
+    }
+}
---
+++ python_script.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This is an example file for python script"""
+
+def my_function(arg):
+    """Main functionality function here"""
+    # write the body of your function here
+    return 'running with %s' % arg
+
+def main():
+    """Run main funcitionality function here.
+    Try to keep main simple.
+    """
+    # run your function through some test cases here
+    # remember: debugging is half the battle!
+    print(my_function('test input'))
+
+if __name__ == '__main__':
+    main()
